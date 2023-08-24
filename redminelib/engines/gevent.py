8effe4c2f67c56c96437f751dbb708a2e0130f8e
Laurier Loiselle <lal@xiphos.com>
2023-08-24 12:52:27 -0400

    engines: gevent: add retries

--- redminelib/engines/gevent.py
+++ redminelib/engines/gevent.py
@@ -4,6 +4,8 @@ Synchronous blocking engine that processes requests one by one.
 
 import grequests
 import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 from . import BaseEngine
 
@@ -12,6 +14,15 @@ class GeventEngine(BaseEngine):
     @staticmethod
     def create_session(**params):
         session = requests.Session()
+        retries = Retry(
+            total=5,
+            backoff_factor=0.1,
+            status_forcelist=[500, 502, 503, 504],
+            raise_on_redirect=True,
+            raise_on_status=True
+        )
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        session.mount('https://', HTTPAdapter(max_retries=retries))
 
         for param in params:
             setattr(session, param, params[param])
@@ -22,8 +33,7 @@ class GeventEngine(BaseEngine):
         if method.lower() == 'get': # Getting, we can use grequests
             unsent_requests = (grequests.get(url, params=params, session=self.session) for params in bulk_params)
             responses = grequests.map(unsent_requests)
-
-            processed_responses = (self.process_response(response)[container] for response in responses)
+            processed_responses = (self.process_response(response)[container] for response in responses if response is not None)
             return [resource for response in processed_responses for resource in response]
         else:
             return [resource for params in bulk_params for resource in self.request(method, url, params=params)[container]]
